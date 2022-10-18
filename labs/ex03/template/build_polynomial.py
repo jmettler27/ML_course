4185f10d7045f7314356d57dc5fdd3032863fcb8
jmettler27 <julien.mettler@epfl.ch>
2022-10-18 09:33:54 +0200

    Clean ex03-04 documentation

--- labs/ex03/template/build_polynomial.py
+++ labs/ex03/template/build_polynomial.py
@@ -5,7 +5,7 @@ import numpy as np
 
 def build_poly(x, degree):
     """polynomial basis functions for input data x, for j=0 up to j=degree."""
-    poly = np.ones((len(x), 1))
+    poly = np.ones(x.shape[0])
     for deg in range(1, degree+1):
         poly = np.c_[poly, x**deg]
     return poly
